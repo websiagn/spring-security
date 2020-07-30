4695819ce8dc17150d93ad3e7becf1125b14607f
youchen <1450207968@qq.com>
2020-07-30 10:00:54 +0800

    定制安全规则以及开启登录、注册

---
+++ src/main/java/com/example/security/config/WebSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.example.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 定制请求的授权规则
+        http.authorizeRequests().antMatchers("/").permitAll()
+                .antMatchers("/level1/**").hasRole("VIP1")
+                .antMatchers("/level2/**").hasRole("VIP2")
+                .antMatchers("/level3/**").hasRole("VIP3");
+
+        // 开启自动配置的登录功能，如果没有登录，没有权限就会来到登录页面
+        http.formLogin();
+
+        // 开启自动配置的注销功能
+        http.logout().logoutSuccessUrl("/");
+    }
+
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("admin")
+                        .password("admin")
+                        .roles("VIP1","VIP2")
+                        .build();
+        UserDetails admin =
+                User.withDefaultPasswordEncoder()
+                        .username("zansan")
+                        .password("zansan")
+                        .roles("VIP3","VIP2")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+}
